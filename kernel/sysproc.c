e00a5a81f68c8802ada7a9a2c8127ae8cc359152
sanazzsanaz <sanazzparviz@gmail.com>
2025-07-11 22:04:20 +0330

    Update sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -5,6 +5,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "logger_custom.h"
 
 uint64
 sys_exit(void)
@@ -12,7 +13,14 @@ sys_exit(void)
   int n;
   argint(0, &n);
   exit(n);
-  return 0; // not reached
+  return 0;  // not reached
+}
+
+uint64
+sys_trigger(void)
+{
+    log_message(INFO, "This is a log to test a new xv6 system call");
+    return 0;
 }
 
 uint64
@@ -43,7 +51,7 @@ sys_sbrk(void)
 
   argint(0, &n);
   addr = myproc()->sz;
-  if (growproc(n) < 0)
+  if(growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -55,14 +63,18 @@ sys_sleep(void)
   uint ticks0;
 
   argint(0, &n);
-  if (n < 0)
+  if(n < 0)
     n = 0;
   acquire(&tickslock);
   ticks0 = ticks;
-  while (ticks - ticks0 < n)
-  {
-    if (killed(myproc()))
-    {
+  if (myproc()->current_thread) {
+    release(&tickslock);
+    sleepthread(n, ticks0); // تابع جدید خواب برای نخ را صدا بزن
+    return 0;
+  }
+
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
       release(&tickslock);
       return -1;
     }
@@ -93,9 +105,32 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+
 uint64
-sys_trigger(void)
-{
-  printf("INFO - This is a log to test a new xv6 system call\n");
-  return 0;
+sys_thread(void) {
+  uint64 start_thread, stack_address, arg;
+  
+  // خواندن آرگومان‌ها از فضای کاربر
+  argaddr(0, &start_thread);
+  argaddr(1, &stack_address);
+  argaddr(2, &arg);
+
+  // فراخوانی تابع اصلی برای تخصیص و ساخت نخ (این تابع را بعدا می‌نویسیم)
+  struct thread *t = allocthread(start_thread, stack_address, arg);
+
+  // اگر نخ با موفقیت ساخته شد، شناسه آن را برگردان
+  return t ? t->id : 0;
+}
+
+// تابع sys_jointhread برای پیوستن به یک نخ
+uint64
+sys_jointhread(void) {
+  int id;
+
+  // خواندن شناسه نخ مورد نظر از فضای کاربر
+  argint(0, &id);
+  
+  // فراخوانی تابع اصلی برای پیوستن به نخ (این تابع را بعدا می‌نویسیم)
+  return jointhread(id);
 }
