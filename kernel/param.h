8ad82a30416efe098164b4cf4f63fee73ce8f647
maryam masoomi <maryam345masoomi@gmail.com>
2025-07-12 11:28:41 +0330

    Update param.h

--- kernel/param.h
+++ kernel/param.h
@@ -1,7 +1,6 @@
 #define NPROC        64  // maximum number of processes
 #define NCPU          8  // maximum number of CPUs
 #define NOFILE       16  // open files per process
-#define NTHREAD       4  //maximum number of threads in each process
 #define NFILE       100  // open files per system
 #define NINODE       50  // maximum number of active i-nodes
 #define NDEV         10  // maximum major device number
@@ -13,3 +12,5 @@
 #define FSSIZE       2000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
 #define USERSTACK    1     // user stack pages
+#define NTHREAD 4 // maximum 
+
